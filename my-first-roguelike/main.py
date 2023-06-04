0570c189c18c3db6beb55919a7e16db518653a0f
Yuri Campos <ygsoares20@gmail.com>
2023-06-04 19:14:58 -0300

    create the terminal, the console and import the actions subclasses and EventHandler class

--- my-first-roguelike/main.py
+++ my-first-roguelike/main.py
@@ -1,4 +1,6 @@
 import tcod
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
 
 def main():
     # Variables that hold the size of our tcod screen
@@ -13,6 +15,16 @@ def main():
     # Reading and Storing the tileset
     tileset = tcod.load_tilesheet('tileset.png', 32, 8, tcod.tileset.CHARMAP_TCOD)
 
+    event_handler = EventHandler()
+
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset=tileset,
+        title='My First Roguelike',
+        vsync=True,
+    )as context:
+        root_console = tcod.console(screen_width,screen_height, order='F')
 
 if __name__ == '__main__':
     main()
