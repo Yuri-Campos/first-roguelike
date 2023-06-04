c87f0dadee306fb9efee6bfcdf25052e687d1f62
Yuri Campos <ygsoares20@gmail.com>
2023-06-04 18:46:12 -0300

    create the tileset, player positon and screen size

--- my-first-roguelike/main.py
+++ my-first-roguelike/main.py
@@ -1,1 +1,18 @@
+import tcod
 
+def main():
+    # Variables that hold the size of our tcod screen
+    screen_width = 80 # this will be read from a JSON file in the future
+    screen_height = 50 # same
+
+    # Player position
+    # Will be the middle of the screen for now
+    player_x = int(screen_width/2) 
+    player_y = int(screen_width/2)
+
+    # Reading and Storing the tileset
+    tileset = tcod.load_tilesheet('tileset.png', 32, 8, tcod.tileset.CHARMAP_TCOD)
+
+
+if __name__ == '__main__':
+    main()
