f1b7e6571376770365385a2c8bc5cb14c0bb8dfb
Yuri Campos <ygsoares20@gmail.com>
2023-06-04 18:55:52 -0300

    defined the basic action subclasses

--- my-first-roguelike/actions.py
+++ my-first-roguelike/actions.py
@@ -1,1 +1,10 @@
+class Action:
+    pass
+class EscapeAction():
+    pass
+class MovementAction():
+    def __init__(self, dx: int, dy: int) -> None:
+        super().__init__()
 
+        self.dx = dx
+        self.dy = dy
