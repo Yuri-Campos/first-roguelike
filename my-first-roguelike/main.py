4058e31be8ed2014c40439abbdd7eef5d3d3f3be
Yuri Campos <ygsoares20@gmail.com>
2023-06-08 01:26:11 -0300

    create the procgen module

--- my-first-roguelike/main.py
+++ my-first-roguelike/main.py
@@ -2,9 +2,8 @@ import tcod
 
 from engine import Engine
 from entity import Entity
-from game_map import GameMap
 from input_handlers import EventHandler
-
+from procgen import generate_dungeon
 
 def main() -> None:
     # Variables that hold the size of our tcod screen
@@ -23,7 +22,7 @@ def main() -> None:
     player = Entity(int(screen_width/2), int(screen_height/2), '@', (255,255,255))
     npc = Entity(int(screen_width/2 - 5), int(screen_height/2), '@', (255,255,0))
 
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(map_width, map_height)
     # Storing the entities in a set 
     entities = {npc, player}
 
